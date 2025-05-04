f990c4e05fed17849815426976b6cab45e7b478f
Hobe <hoppygamer33roblox@gmail.com>
2025-05-04 17:42:57 +0300

    Update stove.js

--- stove.js
+++ stove.js
@@ -1,7 +1,16 @@
-elements.stove = {
-    color: "#808080",
-    behavior: behaviors.WALL,
-    category: "special",
+elements.hello_world = {
+    color: "#ff0000",
+    behavior: [
+        "XX|XX|XX",
+        "XX|FX|XX",
+        "XX|XX|XX",
+        function(pixel) {
+            if (pixel.temp >= 100) {
+                pixel.temp += 10;
+            }
+        },
+    ],
+    category: "land",
     state: "solid",
     burnTemp: 100,
     burnDuration: 3_000_000 * 365.25 * 24 * 60 * 60,
