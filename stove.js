db3b613964812429666ba30040c55d95a928bfc4
Hobe <hoppygamer33roblox@gmail.com>
2025-05-04 17:40:29 +0300

    Create stove.js

---
+++ stove.js
@@ -0,0 +1,8 @@
+elements.stove = {
+    color: "#808080",
+    behavior: behaviors.WALL,
+    category: "special",
+    state: "solid",
+    burnTemp: 100,
+    burnDuration: 3_000_000 * 365.25 * 24 * 60 * 60,
+}
